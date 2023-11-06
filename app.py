8b23fc51169ff9b0154893711676d5ed3a96223c
Kozo Nishida <kozo.nishida@gmail.com>
2023-11-06 18:07:23 +0900

    Update /hibag

--- app.py
+++ app.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 """This is the imputation server web UI."""
 from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
 # set the secret key.  keep this really secret:
 app.secret_key = "k9SZr98j/3yX R~XHH!jmN]0d2,?RT"
 
+data = pd.read_csv('hibagmodel4flask.csv')
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -66,14 +68,31 @@ def bplink():
 
 @app.route("/hibag", methods=["GET", "POST"])
 def hibag():
-    items = ["model1", "model2", "model3"]
-    selected_item = None
+    # items = ["model1", "model2", "model3"]
+    # selected_item = None
 
-    if request.method == "GET":
-        return render_template("hibag.html")
-    elif request.method == "POST":
-        selected_item = request.form.get('select_item')
-    return render_template("hibag.html", items=items, selected_item=selected_item)
+    # if request.method == "GET":
+    #     return render_template("hibag.html")
+    # elif request.method == "POST":
+    #     selected_item = request.form.get('select_item')
+    # return render_template("hibag.html", items=items, selected_item=selected_item)
+
+    selections = [request.form.get('dropdown1'), request.form.get('dropdown2'), request.form.get('dropdown3'), request.form.get('dropdown4')]
+    selections = [s for s in selections if s]  # Remove None values
+
+    filtered_data = data
+    #for i, column in enumerate(['option1', 'option2', 'option3', 'option4']):
+    for i, column in enumerate(['USE_YOUR_MODEL_OR_NOT', 'GENOTYPING_PLATFORMS', 'RESOLUTION', 'ANCESTRY']):
+        if i < len(selections):
+            filtered_data = filtered_data[filtered_data[column] == selections[i]]
+
+    options1 = data['USE_YOUR_MODEL_OR_NOT'].unique().tolist()
+    options2 = filtered_data['GENOTYPING_PLATFORMS'].unique().tolist() if len(selections) >= 1 else []
+    options3 = filtered_data['RESOLUTION'].unique().tolist() if len(selections) >= 2 else []
+    options4 = filtered_data['ANCESTRY'].unique().tolist() if len(selections) >= 3 else []
+    answer = filtered_data['HIBAG_MODEL_URL'].tolist() if len(selections) >= 4 else []
+
+    return render_template('hibag.html', options1=options1, options2=options2, options3=options3, options4=options4, answer=answer, selections=selections)
 
 
 if __name__ == "__main__":
