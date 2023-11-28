8c67fd4c674b244f82890e5b135321c2f4cba344
Kozo Nishida <kozo.nishida@gmail.com>
2023-11-28 17:07:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ hibag_parameters = {
 # set the secret key.  keep this really secret:
 app.secret_key = "k9SZr98j/3yX R~XHH!jmN]0d2,?RT"
 
-data = pd.read_csv('hibagmodel4flask.csv')
+hibagdata = pd.read_csv('hibagmodel4flask.csv')
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -88,26 +88,26 @@ def bplink():
 
 @app.route("/hibag", methods=["GET", "POST"])
 def hibag():
-    # items = ["model1", "model2", "model3"]
-    # selected_item = None
-
-    # if request.method == "GET":
-    #     return render_template("hibag.html")
-    # elif request.method == "POST":
-    #     selected_item = request.form.get('select_item')
-    # return render_template("hibag.html", items=items, selected_item=selected_item)
+    show_text_input = False
+    show_usermodel_jobyml = False
 
     selections = [request.form.get('dropdown1'), request.form.get('dropdown2'), request.form.get('dropdown3'), request.form.get('dropdown4')]
     selections = [s for s in selections if s]  # Remove None values
 
-    filtered_data = data
-    #for i, column in enumerate(['option1', 'option2', 'option3', 'option4']):
+    filtered_data = hibagdata
     for i, column in enumerate(['USE_YOUR_MODEL_OR_NOT', 'GENOTYPING_PLATFORMS', 'RESOLUTION', 'ANCESTRY']):
         if i < len(selections):
             filtered_data = filtered_data[filtered_data[column] == selections[i]]
 
-    options1 = data['USE_YOUR_MODEL_OR_NOT'].unique().tolist()
-    options2 = filtered_data['GENOTYPING_PLATFORMS'].unique().tolist() if len(selections) >= 1 else []
+    options1 = hibagdata['USE_YOUR_MODEL_OR_NOT'].unique().tolist()
+    if len(selections) == 1 and selections[0] == 'Yes':
+        show_text_input = True
+    
+    if 'submit_text' in request.form:
+        hibag_parameters['model_file'] = request.form['text_input']
+        show_usermodel_jobyml = True
+
+    options2 = filtered_data['GENOTYPING_PLATFORMS'].unique().tolist() if len(selections) >= 1 and selections[0] == "No" else []
     options3 = filtered_data['RESOLUTION'].unique().tolist() if len(selections) >= 2 else []
     options4 = filtered_data['ANCESTRY'].unique().tolist() if len(selections) >= 3 else []
     answer = filtered_data['HIBAG_MODEL_URL'].tolist() if len(selections) >= 4 else []
@@ -120,7 +120,7 @@ def hibag():
     rendered_yaml = template.render(hibag_parameters)
     print(rendered_yaml)
 
-    return render_template('hibag.html', options1=options1, options2=options2, options3=options3, options4=options4,
+    return render_template('hibag.html', options1=options1, show_text_input=show_text_input, show_usermodel_jobyml=show_usermodel_jobyml, options2=options2, options3=options3, options4=options4,
                            answer=answer, rendered_yaml=rendered_yaml, selections=selections)
 
 if __name__ == "__main__":
